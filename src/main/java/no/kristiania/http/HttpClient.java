8b7498700de22767cc83e06815dec1274789a952
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-05 17:17:13 +0200

    4/4 test correct

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -3,10 +3,12 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class HttpClient {
 
     private final int statusCode;
+    private HashMap<String, String> headerFields = new HashMap<>();
 
     public HttpClient(String host, int port, String requestTarget) throws IOException {
         Socket socket = new Socket(host, port);
@@ -18,10 +20,16 @@ public class HttpClient {
                         "\r\n").getBytes()
         );
 
-        StringBuilder result = new StringBuilder();
-
         String statusLine = readLine(socket);
         this.statusCode = Integer.parseInt(statusLine.split(" ")[1]);
+
+        String headerLine;
+        while (!(headerLine = readLine(socket)).isBlank()) {
+            int colonPos = headerLine.indexOf(':');
+            String key = headerLine.substring(0, colonPos);
+            String value = headerLine.substring(colonPos+1).trim();
+            headerFields.put(key, value);
+        }
     }
 
     private String readLine(Socket socket) throws IOException {
@@ -31,6 +39,7 @@ public class HttpClient {
         while ((c = in.read()) != -1 && c != '\r') {
             result.append((char) c);
         }
+        in.read();
         return result.toString();
     }
 
@@ -49,6 +58,8 @@ public class HttpClient {
         );
 
 
+
+
         InputStream in = socket.getInputStream();
         int c;
         while ((c = in.read()) != -1){
@@ -56,7 +67,7 @@ public class HttpClient {
         }
     }
 
-    public String getHeader(String s) {
-        return null;
+    public String getHeader(String headerName) {
+        return headerFields.get(headerName);
     }
 }
