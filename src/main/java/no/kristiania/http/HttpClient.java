898757e552d52e8b5b88a9902342481e0796937e
MarcusKri <marcus-kristoffersen@outlook.com>
2021-10-05 13:43:58 +0200

    test shouldReturnStatusCode

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -5,6 +5,14 @@ import java.io.InputStream;
 import java.net.Socket;
 
 public class HttpClient {
+    public HttpClient(String host, int port, String requestTarget) {
+
+    }
+
+    public int getStatusCode() {
+        return 0;
+    }
+
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("httpbin.org", 80);
 
@@ -20,7 +28,5 @@ public class HttpClient {
         while ((c = in.read()) != -1){
             System.out.println((char)c);
         }
-
     }
-
 }
